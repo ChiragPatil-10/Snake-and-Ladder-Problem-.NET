b40ea02f89109c4103b04718f42e566e799d1ee1
Chirag Patil <patilchirag192k@gmail.com>
2025-06-24 14:59:39 +0530

    Added: UC1 PLayer position

--- SnakeAndLadder.cs
+++ SnakeAndLadder.cs
@@ -3,5 +3,10 @@
     public static void Main(string[] args)
     {
         Console.WriteLine("Welcone to Snake and LAdder Simulator");
+        
+        int playerInitialPosition = 0;
+
+        Console.WriteLine("PLayer Initial position in the game is " + playerInitialPosition);
+
     }
 }
