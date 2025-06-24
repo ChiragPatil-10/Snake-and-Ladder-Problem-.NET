6fcaab041ec73e4f02a9b5b0d9fdd06b98c3ea88
Chirag Patil <patilchirag192k@gmail.com>
2025-06-24 16:01:29 +0530

    UC2- player rolls the dice

--- SnakeAndLadder.cs
+++ SnakeAndLadder.cs
@@ -2,11 +2,17 @@
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Welcone to Snake and LAdder Simulator");
+        Console.WriteLine("Welcome to Snake and LAdder Simulator");
         
         int playerInitialPosition = 0;
 
         Console.WriteLine("PLayer Initial position in the game is " + playerInitialPosition);
 
+        Random random = new Random();
+
+        int dieRoll = random.Next(1, 7);
+
+        Console.WriteLine("Player Rolls the die..");
+        Console.WriteLine("Die Shows: " + dieRoll);
     }
 }
